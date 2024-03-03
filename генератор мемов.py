f1ad63d8a6a61aefaf742d6c4073d0c8b3840bbc
ArinaSh1906 <a.shubinskaya@gmail.com>
2024-03-03 22:57:52 +0300

    code is finished

--- генератор мемов.py
+++ генератор мемов.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 print('Генератор мемов запущен!')
-# top_text = input('Введите верхний текст:\n')
+top_text = input('Введите верхний текст:\n')
 bottom_text = input('Введите нижний текст:\n')
 
 print("Список картинок:")
@@ -24,12 +24,16 @@ draw = ImageDraw.Draw(image)
 
 font = ImageFont.truetype('arial.ttf', size = 70)
 
-text = draw.textbbox((0, 0),bottom_text, font)
+text = draw.textbbox((0, 0), top_text, font)
+text_width = text[2]
+
+draw.text(((width - text_width) / 2, 10), top_text, font=font, fill='black')
+
+text = draw.textbbox((0, 0), bottom_text, font)
 text_width = text[2]
 text_height = text[3]
 
-draw.text(((width - text_width) / 2, height - text_height - 10), bottom_text, font = font, fill = 'black')
-
+draw.text(((width - text_width) / 2, height - text_height - 10), bottom_text, font=font, fill='black')
 
 
 image.save(f'{input("Введите название мема: ")}.jpg')
